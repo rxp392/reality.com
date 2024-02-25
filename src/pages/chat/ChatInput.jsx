dc343bd3098ffd18ee86c125d8b92297886dfd27
Z-Youssef <87040650+Z-Youssef@users.noreply.github.com>
2024-02-24 20:29:44 -0500

    better styling; better mock function

--- src/pages/chat/ChatInput.jsx
+++ src/pages/chat/ChatInput.jsx
@@ -3,14 +3,30 @@ import React, { useCallback, useState } from 'react';
 function ChatInput({withUser, username}) {
 
     const [chatMessage, setChatMessage] = useState('');
-    const sendChat = useCallback((message) => {}, [username, withUser]);
+    const [sending, setSending] = useState(false);
+    const sendChat = useCallback(async (message) => {
+        setSending(true);
+        await new Promise(r => setTimeout(r, 2000));
+        alert(`Sent: ${message}`);
+        setChatMessage('');
+        setSending(false);
+    }, [setChatMessage, setSending/* withUser, username */]);
 
     return(
         <div
             className='chat-input'
         >
-            <textarea className='input' placeholder='Type a message' />
-            <button onClick={() => sendChat(chatMessage)}> 
+            <textarea 
+                className='input'
+                placeholder='Type a message' 
+                value={chatMessage}
+                onChange={event => setChatMessage(event.target.value)}
+            />
+            <button 
+                className='send'
+                onClick={() => sendChat(chatMessage)}
+                disabled={sending || chatMessage.length === 0}
+            > 
                 this will look cool at some point
             </button>
         </div>
