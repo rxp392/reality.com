3706733ac08bfb0945fa3ce811840970135d93ed
Z-Youssef <87040650+Z-Youssef@users.noreply.github.com>
2024-03-03 14:29:04 -0500

    generify feed component

---
+++ src/pages/base/GenericFeed.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+function GenericFeed({ fetchFunction, fetchFuncName, dependentInfo, className, resultMapping, emptyMsg }) {
+        
+    const fetchResults = useCallback(async (pageParam) => {
+        const results = await fetchFunction(pageParam, dependentInfo);
+        return { results, nextPage: pageParam + 1, totalPages: 1 };
+    }, [fetchFunction, dependentInfo]);
+
+    const {
+        data,
+        isLoading,
+        isError,
+        hasNextPage,
+        fetchNextPage
+    } = useInfiniteQuery([fetchFuncName, dependentInfo], 
+        ({ pageParam = 1 }) => fetchResults(pageParam), 
+        {
+            refetchOnMount: true,
+            refetchOnWindowFocus: false,
+            getNextPageParam: (lastPage) => (lastPage.nextPage < lastPage.totalPages) ? lastPage.nextPage : undefined
+        }
+    );
+
+    return (
+        <>
+            {isLoading ? (
+                    <p>Loading...</p>
+                ) : isError ? (
+                        <p>There was an error</p>
+                    ) : ((!!data && !!(data.pages) && !!(data.pages[0]) && !!(data.pages[0].results) && !!(data.pages[0].results[0])) ?
+                            <InfiniteScroll 
+                                hasMore={hasNextPage} 
+                                loadMore={fetchNextPage} 
+                                reversed={true}
+                                className={className}
+                            >
+                                {data.pages.map(page => page.results.map(info => resultMapping(info)))}
+                            </InfiniteScroll>
+                            :
+                            <h3>{emptyMsg}</h3>
+                    )
+            }
+        </>
+    );
+}
+
+export default GenericFeed;
--- src/pages/chat/ChatFeed.jsx
+++ src/pages/chat/ChatFeed.jsx
@@ -1,50 +1,18 @@
-import React, { useCallback } from 'react';
-import InfiniteScroll from 'react-infinite-scroller';
-import { useInfiniteQuery } from 'react-query';
+import React from 'react';
 import ChatMessage from './ChatMessage';
+import GenericFeed from '../base/GenericFeed';
 
 function ChatFeed({ fetchFunction, user }) {
-    
-    const fetchMessages = useCallback(async (pageParam) => {
-        const results = await fetchFunction(pageParam, user);
-        return { results, nextPage: pageParam + 1, totalPages: 1 };
-    }, [fetchFunction, user]);
-
-    const {
-        data,
-        isLoading,
-        isError,
-        hasNextPage,
-        fetchNextPage
-    } = useInfiniteQuery(['messages', user], 
-        ({ pageParam = 1 }) => fetchMessages(pageParam), 
-        {
-            refetchOnMount: true,
-            refetchOnWindowFocus: false,
-            getNextPageParam: (lastPage) => (lastPage.nextPage < lastPage.totalPages) ? lastPage.nextPage : undefined
-        }
-    );
 
     return (
-        <>
-            {isLoading ? (
-                    <p>Loading...</p>
-                ) : isError ? (
-                        <p>There was an error</p>
-                    ) : ((!!data && !!(data.pages) && !!(data.pages[0]) && !!(data.pages[0].results) && !!(data.pages[0].results[0])) ?
-                            <InfiniteScroll 
-                                hasMore={hasNextPage} 
-                                loadMore={fetchNextPage} 
-                                reversed={true}
-                                className='chat-feed'
-                            >
-                                {data.pages.map(page => page.results.map(info => <ChatMessage key={info.timestamp} {...info} />))}
-                            </InfiniteScroll>
-                            :
-                            <h3>Start chatting!</h3>
-                    )
-            }
-        </>
+        <GenericFeed 
+            fetchFunction={fetchFunction} 
+            fetchFuncName={'messages'} 
+            dependentInfo={user} 
+            className={'chat-feed'}
+            resultMapping={info => <ChatMessage key={info.timestamp} {...info} />}
+            emptyMsg={'Start chatting!'}
+        />
     );
 }
 
