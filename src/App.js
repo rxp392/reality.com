256dac1bd84d65830e404a335f48ff35abb6d0c2
Z-Youssef <87040650+Z-Youssef@users.noreply.github.com>
2024-03-10 10:54:21 -0400

    formatting

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { RouterProvider } from 'react-router-dom';
-import {  QueryClient, QueryClientProvider } from 'react-query';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 import routes from './routes';
 
